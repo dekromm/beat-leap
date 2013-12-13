14032fcc0e1398d6c7bfba767da6611a02817edf
ivanbravi <ivan.bravi@gmail.com>
2013-12-13 11:09:11 +0100

    Menù

---
+++ Assets/Scripts/LoadLevel.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoadLevel : MonoBehaviour {
+
+	void OnMouseDown(){
+		Game.Current().SetLevel(gameObject.name);
+		Application.LoadLevel("Game");
+	}
+
+}
