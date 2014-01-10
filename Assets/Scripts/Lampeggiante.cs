416b0f6fe849f5062a19970989626d9ef8d3ad46
Guido <gerosa1990@yahoo.it>
2014-01-10 18:41:13 +0100

    Introdotto lampeggiante; anche avvicinato lo speaker alla camera (boost al volume)

---
+++ Assets/Scripts/Lampeggiante.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Lampeggiante : MonoBehaviour
+{
+	
+	public float blinkTime = 0.075f;
+	private bool hasToBlink;
+	private float blinkCount;
+	// Use this for initialization
+	void Start()
+	{
+		hasToBlink = false;
+	}
+
+
+	// Update is called once per frame
+	void Update()
+	{
+		if (hasToBlink) {
+			this.renderer.enabled = true;
+			blinkCount -= Time.deltaTime;
+		} 
+		if (hasToBlink && blinkCount < 0) {
+			hasToBlink = false;
+			renderer.enabled = false;
+			Debug.Log(Time.deltaTime);
+		}
+	}
+
+	public void Blink()
+	{
+		hasToBlink = true;
+		blinkCount = blinkTime;
+		Debug.Log("blink!");
+	}
+}
