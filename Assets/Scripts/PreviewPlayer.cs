1d0b380819d1e690695135fbaeed49c442b10d3c
Tunarock <ste.tunarock@gmail.com>
2014-01-10 12:44:55 +0100

    preview clip audio nel menù Title

---
+++ Assets/Scripts/PreviewPlayer.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PreviewPlayer : MonoBehaviour {
+
+	AudioSource audiosrc;
+	string src;
+
+	// Use this for initialization
+	void Start () {
+
+		audiosrc = (AudioSource) gameObject.GetComponent<AudioSource>();
+
+		src = gameObject.name;
+
+		audiosrc.clip = Resources.Load("Songs/" + src, typeof(AudioClip)) as AudioClip;
+	
+	}
+
+	void OnMouseEnter() {
+		audiosrc.Play();	
+	}
+
+	void OnMouseExit() {
+		audiosrc.Stop();	
+	}
+}
