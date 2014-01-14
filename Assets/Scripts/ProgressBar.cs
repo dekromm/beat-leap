5175b459b3d974ac25bf314c635b97bcd9192e0d
Tunarock <ste.tunarock@gmail.com>
2014-01-14 10:43:57 +0100

    ProgressBar & bug fix

---
+++ Assets/Scripts/ProgressBar.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ProgressBar : MonoBehaviour {
+	
+	private AudioSource song;
+	private AudioClip audio;
+	private float length;
+	private float currentTime;
+
+	private const float sizeX = 40;
+	private const float startOffset = 35;
+	
+	void Start () {
+	
+		song = GameObject.Find("Speaker").GetComponent ("AudioSource") as AudioSource;
+		audio = Resources.Load("Songs/" + Game.Current().Level(), typeof(AudioClip)) as AudioClip;
+		length = audio.length;
+	}
+
+	void Update () {
+
+		currentTime = song.time;
+		Vector3 old = gameObject.transform.position;
+		gameObject.transform.position = new Vector3(GetX (),old.y,old.z);
+	}
+
+	private float GetX(){
+
+		float ratio = currentTime / length;
+
+		return ratio*sizeX + startOffset;
+	}
+}
