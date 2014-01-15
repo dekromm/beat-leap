31b62c25c3b55db7fd75730353d97acbf76d0252
ivanbravi <ivan.bravi@gmail.com>
2014-01-15 23:31:07 +0100

    Effetti scudo ed esplosione + LightConeBeat

---
+++ Assets/Scripts/InputSetup.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InputSetup : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		if(Game.Current().isMobile){
+			GameObject swipe = GameObject.Find("_SwipeInput");
+			GameObject down = GameObject.Find("Down");
+			GameObject left = GameObject.Find("Left");
+			GameObject right = GameObject.Find("Right");
+			GameObject up = GameObject.Find("Up");
+			GameObject center = GameObject.Find("Center");
+
+			if(Game.Current().swipeInput){
+				swipe.SetActive(true);
+
+				center.SetActive(true);
+
+				up.SetActive(false);
+				right.SetActive(false);
+				left.SetActive(false);
+				down.SetActive(false);
+			}else{
+				swipe.SetActive(false);
+				
+				center.SetActive(true);
+				
+				up.SetActive(true);
+				right.SetActive(true);
+				left.SetActive(true);
+				down.SetActive(true);
+			}
+		}
+
+	}
+}
