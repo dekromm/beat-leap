f6a766ec5b11ea897c6c6663819702bf66dd7fe5
ivanbravi <ivan.bravi@gmail.com>
2014-01-22 08:57:17 +0100

    Menù navigabili/Info

---
+++ Assets/Scripts/UserInterface/Navigation/Navigable.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Navigable : MonoBehaviour {
+
+	private GameObject select;
+
+	private bool initialized;
+
+	void Start () {
+		if(!initialized){
+			Init();
+		}
+	}
+
+	public void Select(){
+		if(select!=null){
+			select.SetActive(true);
+		}else{
+			Init();
+		}
+	}
+
+	public void Deselect(){
+		select.SetActive(false);
+	}
+
+	virtual public void Action(){
+
+	}
+
+	virtual public void Init(){
+
+		if(!initialized){
+			Transform childSelectTransform = gameObject.transform.FindChild("selection");
+			select = childSelectTransform.gameObject;
+			Deselect();
+			initialized = true;
+		}
+	}
+}
---
+++ Assets/Scripts/UserInterface/Navigation/QuitNavigable.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class QuitNavigable : Navigable {
+	
+	void Start () {
+		Init();	
+	}
+
+	override public void Action(){
+		Application.Quit();
+	}
+
+}
