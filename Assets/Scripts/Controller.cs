128a344964b7fb584bc29e200aae5e01d0441462
Guido <gerosa1990@yahoo.it>
2014-01-23 13:28:45 +0100

    Start del gamepad funzionante

--- Assets/Scripts/Controller.cs
+++ Assets/Scripts/Controller.cs
@@ -3,15 +3,15 @@ using System.Collections;
 
 public class Controller : MonoBehaviour
 {
-		public GameMechanics gameMechanics;
-		const KeyCode UP = KeyCode.W;
-		const KeyCode DOWN = KeyCode.S;
-		const KeyCode RIGHT = KeyCode.D;
-		const KeyCode LEFT = KeyCode.A;
-		const KeyCode PAUSE = KeyCode.Space;
-		private bool isNotDown;
-		private bool isJoyPadNotDown;
-		private bool isPause;
+	public GameMechanics gameMechanics;
+	const KeyCode UP = KeyCode.W;
+	const KeyCode DOWN = KeyCode.S;
+	const KeyCode RIGHT = KeyCode.D;
+	const KeyCode LEFT = KeyCode.A;
+	const KeyCode PAUSE = KeyCode.Space;
+	private bool isNotDown;
+	private bool isJoyPadNotDown;
+	private bool isPause;
 
 //	const KeyCode UP = KeyCode.UpArrow;
 //	const KeyCode DOWN = KeyCode.DownArrow;
@@ -20,155 +20,156 @@ public class Controller : MonoBehaviour
 //	const KeyCode PAUSE = KeyCode.Space;
 
 	
-		// Use this for initialization
-		void Start ()
-		{
-				string levelName = Game.Current ().Level ();
-				gameMechanics = new GameMechanics (levelName);
-				this.SwitchPauseResume ();
-				Input.GetAxis ("Horizontal");
-				isNotDown = true;
-				isPause = false;
-		}
+	// Use this for initialization
+	void Start()
+	{
+		string levelName = Game.Current().Level();
+		gameMechanics = new GameMechanics(levelName);
+		this.SwitchPauseResume();
+		Input.GetAxis("Horizontal");
+		isNotDown = true;
+		isPause = false;
+	}
         
-		// Update is called once per frame
-		void Update ()
-		{
-				bool x = Input.GetButton ("X");
-				bool a = Input.GetButtonDown ("A");
-				bool y = Input.GetButtonDown ("Y");
-				bool b = Input.GetButtonDown ("B");
-				bool pause = Input.GetButtonDown ("JPause");
+	// Update is called once per frame
+	void Update()
+	{
+		bool x = Input.GetButton("X");
+		bool a = Input.GetButtonDown("A");
+		bool y = Input.GetButtonDown("Y");
+		bool b = Input.GetButtonDown("B");
+		bool pause = Input.GetButtonDown("JPause");
 			
-				if (gameMechanics.isGamePlaying) {
+		if (gameMechanics.isGamePlaying) {
 
-						if (x) {
-								if (isJoyPadNotDown) 
-										gameMechanics.MoveLeft ();								
-								isJoyPadNotDown = false;
+			if (x) {
+				if (isJoyPadNotDown) 
+					gameMechanics.MoveLeft();								
+				isJoyPadNotDown = false;
 											                      
-						} else if (b) {
-								if (isJoyPadNotDown) 
-										gameMechanics.MoveRight ();
-								isJoyPadNotDown = false;
+			} else if (b) {
+				if (isJoyPadNotDown) 
+					gameMechanics.MoveRight();
+				isJoyPadNotDown = false;
 								
 			
-						} else if (a) {
-								if (isJoyPadNotDown) 
-										gameMechanics.MoveDown ();
-								isJoyPadNotDown = false;
+			} else if (a) {
+				if (isJoyPadNotDown) 
+					gameMechanics.MoveDown();
+				isJoyPadNotDown = false;
 								
 			
-						} else if (y) {
-								if (isJoyPadNotDown) 
-										gameMechanics.MoveUp ();
-								isJoyPadNotDown = false;
+			} else if (y) {
+				if (isJoyPadNotDown) 
+					gameMechanics.MoveUp();
+				isJoyPadNotDown = false;
 								
-						} else
-								isJoyPadNotDown = true;
-				}
-				if (pause) {
-						if (!isPause) {
-							if(gameMechanics.isGamePlaying){
-								gameMechanics.SwitchPauseResume ();
-								isPause = true;	
-							}
-						}
-				} else
-						isPause = false;
-
+			} else
+				isJoyPadNotDown = true;
 		}
+		if (pause) {
+			if (!isPause) {
+				if (gameMechanics.isGamePlaying) {
+					gameMechanics.SwitchPauseResume();
+					isPause = true;	
+				}
+			}
+		} else
+			isPause = false;
+
+	}
         
 //		 OnGUI is called once for every event in unity
 //		 this means it is frame-indepent, making it the right place
 //		 where to put the input capture. Maybe.
-		void OnGUI ()
-		{
-				if (Event.current.type.Equals (EventType.KeyDown) && isNotDown) {
+	void OnGUI()
+	{
+		if (Event.current.type.Equals(EventType.KeyDown) && isNotDown) {
 
-						isNotDown = false;
+			isNotDown = false;
 
-						float vertical = Input.GetAxis ("Vertical");
-						float horizontal = Input.GetAxis ("Horizontal");
-						float pause = Input.GetAxis ("Pause");
+			float vertical = Input.GetAxis("Vertical");
+			float horizontal = Input.GetAxis("Horizontal");
+			bool pause = Input.GetButtonDown("Pause");
 												
-						if (gameMechanics.isGamePlaying) {
+			if (gameMechanics.isGamePlaying) {
 									
-								if (vertical > 0) {
+				if (vertical > 0) {
                 
-										gameMechanics.MoveUp ();
+					gameMechanics.MoveUp();
                         
-								} else if (vertical < 0) {
+				} else if (vertical < 0) {
                 
-										gameMechanics.MoveDown ();
+					gameMechanics.MoveDown();
                         
-								} else if (horizontal < 0) {
+				} else if (horizontal < 0) {
                 
-										gameMechanics.MoveLeft ();
+					gameMechanics.MoveLeft();
                         
-								} else if (horizontal > 0) {
+				} else if (horizontal > 0) {
                                 
-										gameMechanics.MoveRight ();
+					gameMechanics.MoveRight();
                                         
-								}
-						}
-						if (pause == 1 && gameMechanics.isGamePlaying)                
-								SwitchPauseResume ();                                        
-
 				}
-				if (Event.current.type.Equals (EventType.KeyUp))
-						isNotDown = true;
-		}
-
-		public void SwitchPauseResume ()
-		{
-
-				if (!gameMechanics.isGamePlaying) {
-						StartTimer ();
-				} else
-						stopTimer ();
-		
-				//GameMechanics.StartPlaying();
-		
-				gameMechanics.SwitchPauseResume ();
-
-		}
-
-		private void StartTimer ()
-		{
-
-				InvokeRepeating ("CheckBeat", 0, 0.001f);
-
-		}
-
-		public void GameOver ()
-		{
-				stopTimer ();
-				gameMechanics.GameOver ();
-				Invoke ("GameOverScene", 4.0f);
-		}
-
-
-		public void GameOverScene(){
-			if(Game.Current().isMobile){
-				Application.LoadLevel("GameOver_ScoreSolo");
-			}else{
-				Application.LoadLevel("GameOver");
 			}
-		}
-
-		public void stopTimer ()
-		{
-
-				CancelInvoke ("CheckBeat");
+			if (pause && gameMechanics.isGamePlaying) {            
+				SwitchPauseResume();                
+			}
 
 		}
+		if (Event.current.type.Equals(EventType.KeyUp))
+			isNotDown = true;
+	}
 
-		private void CheckBeat ()
-		{
+	public void SwitchPauseResume()
+	{
 
-				gameMechanics.CheckBeat ();
+		if (!gameMechanics.isGamePlaying) {
+			StartTimer();
+		} else
+			stopTimer();
+		
+		//GameMechanics.StartPlaying();
+		
+		gameMechanics.SwitchPauseResume();
+
+	}
+
+	private void StartTimer()
+	{
+
+		InvokeRepeating("CheckBeat", 0, 0.001f);
+
+	}
+
+	public void GameOver()
+	{
+		stopTimer();
+		gameMechanics.GameOver();
+		Invoke("GameOverScene", 4.0f);
+	}
+
+	public void GameOverScene()
+	{
+		if (Game.Current().isMobile) {
+			Application.LoadLevel("GameOver_ScoreSolo");
+		} else {
+			Application.LoadLevel("GameOver");
+		}
+	}
+
+	public void stopTimer()
+	{
+
+		CancelInvoke("CheckBeat");
+
+	}
+
+	private void CheckBeat()
+	{
+
+		gameMechanics.CheckBeat();
 			
-		}
+	}
     
 }
